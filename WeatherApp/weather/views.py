5ba9a1768a2b79a71e899529149956770b8edea1
Mavalliss <a.m.popugaev@gmail.com>
2020-03-09 20:20:16 +0300

    Choose every country that you want.

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 import requests
+from .models import City
+from .forms import CityForm
 
 
 # Create your views here.
@@ -7,15 +9,25 @@ import requests
 def index(request):
     appid = "82b797b6ebc625032318e16f1b42c016"
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
-    city = "London"
+    cities = City.objects.all()
 
-    res = requests.get(url.format(city)).json()
-    city_info = {
-        'city': city,
-        'temp': res["main"]["temp"],
-        'icon': res["weather"][-1]["icon"]
-    }
+    if (request.method == "POST"):
+        form = CityForm(request.POST)
+        form.save()
 
-    conext = {'info': city_info}
+    form = CityForm()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][-1]["icon"]
+        }
+        all_cities.append(city_info)
+
+    conext = {'all_info': all_cities, 'form': form}
 
     return render(request, 'weather/index.html', conext)
