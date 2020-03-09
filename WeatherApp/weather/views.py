7302450f208046e7bbe2b2a05fdc73a0f2c59e52
Mavalliss <a.m.popugaev@gmail.com>
2020-03-09 18:53:04 +0300

    I can show weather in country, that I write in HTML.

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
+
 
 # Create your views here.
+
+def index(request):
+    appid = "82b797b6ebc625032318e16f1b42c016"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    city = "London"
+
+    res = requests.get(url.format(city)).json()
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][-1]["icon"]
+    }
+
+    conext = {'info': city_info}
+
+    return render(request, 'weather/index.html', conext)
